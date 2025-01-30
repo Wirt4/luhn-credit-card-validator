82ad340f6cbd84ac2f89a20e68036abff69e7656
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-01-29 23:52:08 -0800

    green-test

--- packages/luhn/luhn.go
+++ packages/luhn/luhn.go
@@ -1,9 +1,72 @@
 package luhn
 
-import "main.go/packages/interfaces"
+import (
+	"main.go/packages/interfaces"
+)
 
 func IsValid(sequence interfaces.DigitSequence) bool {
-	seq := sequence.GetSequence()
-	lastDigit := seq[len(seq)-1]
-	return lastDigit == 5
+	stack := newStackFromSequence(sequence.GetSequence())
+	check_digit := stack.pop()
+	return check_digit == calculateCheckDigit(stack)
+}
+
+func calculateCheckDigit(stack *stack) int {
+	odd := stack.size()%2 != 0
+	sum := 0
+	for !stack.isEmpty() {
+		digit := stack.pop()
+		if odd {
+			sum += digit
+		} else {
+			sum += sumDigit(digit)
+		}
+		odd = !odd
+	}
+	return (10 - (sum % 10)) % 10
+}
+
+func sumDigit(digit int) int {
+	digit *= 2
+	if digit > 9 {
+		return addDigits(digit)
+	}
+	return digit
+}
+
+func addDigits(digit int) int {
+	sum := 0
+	for digit > 0 {
+		sum += digit % 10
+		digit /= 10
+	}
+	return sum
+}
+
+type stack struct {
+	sequence []int
+}
+
+func (s *stack) size() int {
+	return len(s.sequence)
+}
+
+func newStackFromSequence(sequence []int) *stack {
+	s := &stack{
+		sequence: sequence,
+	}
+	return s
+}
+
+func (s *stack) isEmpty() bool {
+	return len(s.sequence) == 0
+}
+
+func (s *stack) pop() int {
+	if len(s.sequence) == 0 {
+		return -1
+	}
+	lastIndex := len(s.sequence) - 1
+	lastDigit := s.sequence[lastIndex]
+	s.sequence = s.sequence[:lastIndex]
+	return lastDigit
 }
--- packages/luhn/luhn_test.go
+++ packages/luhn/luhn_test.go
@@ -14,7 +14,7 @@ func (m *mockDigitSeqence) GetSequence() []int {
 	return m.sequence
 }
 
-func TestIsValid(t *testing.T) {
+func TestIsValidSmallCase(t *testing.T) {
 	mockNumbers := &mockDigitSeqence{
 		sequence: []int{1, 2, 5},
 	}
@@ -35,5 +35,17 @@ func TestIsInvalid(t *testing.T) {
 
 	if actual != false {
 		t.Errorf("%v should not be valid", mockNumbers.GetSequence())
+	}
+}
+
+func TestIsValid(t *testing.T) {
+	mockNumbers := &mockDigitSeqence{
+		sequence: []int{1, 7, 8, 9, 3, 7, 2, 9, 9, 7, 4},
+	}
+
+	actual := IsValid(mockNumbers)
+
+	if actual != true {
+		t.Errorf("%v should be valid", mockNumbers.GetSequence())
 	}
 }
