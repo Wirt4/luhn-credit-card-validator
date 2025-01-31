473a71ff140f845147f84fba3d356499cd68757b
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2025-01-31 13:42:50 -0800

    listenable functionality -- hot damn

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-// TODO
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"main.go/packages/handlers"
+	"main.go/packages/luhn"
+)
+
 func main() {
+	handler := handlers.NewHandler(&luhn.LuhnValidator{})
+	http.HandleFunc("/validate_credit_card/", handler.HandleRequest)
+	fmt.Println("Server is listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
