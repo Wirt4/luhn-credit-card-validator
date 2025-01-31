fd10e73809e6a412a7c3888af79f733e08ed09e9
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2025-01-30 19:38:11 -0800

    simplify

--- packages/handlers/handlers.go
+++ packages/handlers/handlers.go
@@ -3,8 +3,6 @@ package handlers
 import "net/http"
 
 func HandleGetRequest(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Only GET requests are allowed\n"))
-	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	w.Write([]byte("Only GET requests are allowed\n"))
 }
