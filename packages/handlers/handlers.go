867ee2051d61ec51c81bc3ff33866a3326d41840
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2025-01-30 19:37:29 -0800

    green

--- packages/handlers/handlers.go
+++ packages/handlers/handlers.go
@@ -2,4 +2,9 @@ package handlers
 
 import "net/http"
 
-func HandleGetRequest(w http.ResponseWriter, r *http.Request) {}
+func HandleGetRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Only GET requests are allowed\n"))
+	}
+}
