fa3a59719a132749c0d0622ef3131b8d0d7b931c
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-02-04 01:01:13 -0800

    tricksy

--- packages/card_issuer/card_issuer.go
+++ packages/card_issuer/card_issuer.go
@@ -10,43 +10,60 @@ type CardIssuer struct {
 	Issuer string
 }
 
-type Node struct {
-	Children map[int]*Node
+func GetCardIssuers(sequence []int) []CardIssuer {
+	issuer_directory := buildTree()
+	v := visitor{sequence: sequence, ndx: 0, cur: issuer_directory, visited: []CardIssuer{}}
+	v.traverse()
+	return v.visited
+}
+
+type node struct {
+	Children map[int]*node
 	Data     *CardIssuer
 }
 
-func insertNode(iin string, issuer *CardIssuer, n *Node) *Node {
-	if n == nil {
-		n = &Node{Children: nil}
+func buildTree() *node {
+	tree := newTree()
+	tree.insert("VISA", 4, 16)
+	for _, iin := range []int{34, 37} {
+		tree.insert("American Express", iin, 15)
 	}
-	if iin == "" {
-		n.Data = issuer
-	} else {
-		msd := int(iin[0] - '0')
-		if len(iin) > 1 {
-			iin = iin[1:]
-		} else {
-			iin = ""
-		}
-		if n.Children == nil {
-			n.Children = make(map[int]*Node)
-		}
-		n.Children[msd] = insertNode(iin, issuer, n.Children[msd])
+	tree.insert("China T-Union", 31, 19)
+	for _, iin := range []int{60, 65, 81, 82, 508, 353, 356} {
+		tree.insert("RuPay", iin, 16)
 	}
-	return n
-}
+	tree.insert("BORICA-BANKCARD", 25, 16)
+	for i := 2221; i <= 2720; i++ {
+		tree.insert("Mastercard", i, 16)
+	}
+	for i := 51; i <= 59; i++ {
+		var provider = "Mastercard"
+		if i == 55 {
+			provider += ": Diners Club U.S. and Canada"
+		}
+		tree.insert(provider, i, 16)
+	}
+	for _, iin := range []int{65, 9792} {
+		tree.insert("Troy", iin, 16)
+	}
+	tree.insert("UATP", 1, 15)
+	tree.insert("LankaPay", 357111, 16)
 
-func GetCardIssuers(sequence []int) []CardIssuer {
-	n := buildTree()
-	v := visitor{sequence: sequence, ndx: 0, cur: n, visited: []CardIssuer{}}
-	v.traverse()
-	return v.visited
+	for _, iin := range []int{4026, 417500, 4508, 4844, 4913, 4917} {
+		tree.insert("Visa Electron", iin, 16)
+	}
+	for _, iin := range []int{5019, 4571} {
+		tree.insert("Dankort", iin, 16)
+	}
+
+	tree.insertRange("China Union Pay", 62, 16, 19)
+	return tree.Root
 }
 
 type visitor struct {
 	sequence []int
 	ndx      int
-	cur      *Node
+	cur      *node
 	visited  []CardIssuer
 }
 
@@ -60,55 +77,19 @@ func (v *visitor) traverse() {
 	}
 }
 
-func buildTree() *Node {
-	tree := newTree()
-	tree.Insert("VISA", 4, 16)
-	for _, iin := range []int{34, 37} {
-		tree.Insert("American Express", iin, 15)
-	}
-	tree.Insert("China T-Union", 31, 19)
-	for _, iin := range []int{60, 65, 81, 82, 508, 353, 356} {
-		tree.Insert("RuPay", iin, 16)
-	}
-	tree.Insert("BORICA-BANKCARD", 25, 16)
-	for i := 2221; i <= 2720; i++ {
-		tree.Insert("Mastercard", i, 16)
-	}
-	for i := 51; i <= 59; i++ {
-		var provider = "Mastercard"
-		if i == 55 {
-			provider += ": Diners Club U.S. and Canada"
-		}
-		tree.Insert(provider, i, 16)
-	}
-	for _, iin := range []int{65, 9792} {
-		tree.Insert("Troy", iin, 16)
-	}
-	tree.Insert("UATP", 1, 15)
-	tree.Insert("LankaPay", 357111, 16)
-
-	for _, iin := range []int{4026, 417500, 4508, 4844, 4913, 4917} {
-		tree.Insert("Visa Electron", iin, 16)
-	}
-	for _, iin := range []int{5019, 4571} {
-		tree.Insert("Dankort", iin, 16)
-	}
-
-	tree.Insert("VISA", 4, 16)
-
-	tree.InsertRange("China Union Pay", 62, 16, 19)
-	return tree.Root
-}
-
 type tree struct {
-	Root *Node
+	Root *node
 }
 
-func (t *tree) Insert(issuerName string, iin int, sequenceLength int) {
-	t.InsertRange(issuerName, iin, sequenceLength, sequenceLength)
+func newTree() *tree {
+	return &tree{Root: &node{Children: nil}}
 }
 
-func (t *tree) InsertRange(issuerName string, iin int, start int, end int) {
+func (t *tree) insert(issuerName string, iin int, sequenceLength int) {
+	t.insertRange(issuerName, iin, sequenceLength, sequenceLength)
+}
+
+func (t *tree) insertRange(issuerName string, iin int, start int, end int) {
 	t.Root = insertNode(
 		strconv.Itoa(iin),
 		&CardIssuer{
@@ -119,6 +100,23 @@ func (t *tree) InsertRange(issuerName string, iin int, start int, end int) {
 		t.Root)
 }
 
-func newTree() *tree {
-	return &tree{Root: &Node{Children: nil}}
+func insertNode(iin string, issuer *CardIssuer, n *node) *node {
+	if n == nil {
+		n = &node{Children: nil}
+	}
+	if iin == "" {
+		n.Data = issuer
+	} else {
+		msd := int(iin[0] - '0')
+		if len(iin) > 1 {
+			iin = iin[1:]
+		} else {
+			iin = ""
+		}
+		if n.Children == nil {
+			n.Children = make(map[int]*node)
+		}
+		n.Children[msd] = insertNode(iin, issuer, n.Children[msd])
+	}
+	return n
 }
