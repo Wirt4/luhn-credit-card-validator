14a3052cafccdf654775807447599d626a665135
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-02-06 22:46:06 -0800

    green

--- packages/card_issuer/company_directory.go
+++ packages/card_issuer/company_directory.go
@@ -1,6 +1,7 @@
 package card_issuer
 
 import (
+	"container/list"
 	"strconv"
 	"strings"
 	"sync"
@@ -116,20 +117,50 @@ func (i *numberHandler) Range() []int {
 	return i.data
 }
 
+func (i *numberHandler) stringisArray(s string) bool {
+	return strings.Contains(s, "[") && strings.Contains(s, "]")
+}
+
+func (i *numberHandler) stringIsRange(s string) bool {
+	return strings.Contains(s, "-")
+}
+
+func (i *numberHandler) parseNumber(s string) int {
+	num, _ := strconv.Atoi(s)
+	return num
+}
+
 func (i *numberHandler) Set(s string) {
-	//case one, is not bracketed
-	if !strings.Contains(s, "[") && !strings.Contains(s, "-") {
-		num, _ := strconv.Atoi(s)
-		i.data = []int{num}
-		return
+	q := newQueue()
+	q.Add(s)
+	numbers := []int{}
+	for !q.isEmpty() {
+		current := q.DeQueue()
+		if i.stringisArray(current) {
+			current = strings.TrimPrefix(current, "[")
+			current = strings.TrimSuffix(current, "]")
+			if !strings.Contains(current, ",") {
+				q.Add(current)
+				continue
+			}
+			contents := strings.Split(current, ", ")
+			for _, v := range contents {
+				q.Add(v)
+			}
+			continue
+		}
+		if i.stringIsRange(current) {
+			r := strings.Split(current, "-")
+			low := i.parseNumber(r[0])
+			high := i.parseNumber(r[1])
+			for j := low; j <= high; j++ {
+				numbers = append(numbers, j)
+			}
+			continue
+		}
+		numbers = append(numbers, i.parseNumber(current))
 	}
-	//case: values are hypen-separated
-	num_strings := strings.Split(s, "-")
-	low, _ := strconv.Atoi(num_strings[0])
-	high, _ := strconv.Atoi(num_strings[1])
-	for j := low; j <= high; j++ {
-		i.data = append(i.data, j)
-	}
+	i.data = numbers
 }
 
 func (i *numberHandler) GetLow() int {
@@ -138,4 +169,27 @@ func (i *numberHandler) GetLow() int {
 
 func (i *numberHandler) GetHigh() int {
 	return 16
+}
+
+type queue struct {
+	*list.List
+}
+
+func (q *queue) Add(v string) {
+	q.PushBack(v)
+}
+
+func (q *queue) isEmpty() bool {
+	return q.Len() == 0
+}
+
+func (q *queue) DeQueue() string {
+	e := q.Front()
+	q.List.Remove(e)
+	return e.Value.(string)
+}
+
+// New is a new instance of a Queue
+func newQueue() queue {
+	return queue{list.New()}
 }
